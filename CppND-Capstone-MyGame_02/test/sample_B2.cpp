7619aa1cabae8428b8753d533c1d13ee22ce9c72
est2mzd <est2mzd@gmail.com>
2020-08-15 19:54:25 +0900

    update modifying files

---
+++ CppND-Capstone-MyGame_02/test/sample_B2.cpp
@@ -0,0 +1,12 @@
+#include "sample_B.h"
+#include <iostream>
+
+sample_B::sample_B()
+{
+    std::cout << "sample B" << std::endl;
+}
+
+void sample_B::echo()
+{
+    std::cout << "sample_B : echo" << std::endl;
+}
---
+++ CppND-Capstone-MyGame_02/test/test2.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "sample_B2.h"
+int main()
+{
+    sample_B obj_B;
+    obj_B.obj_A.echo();
+}
